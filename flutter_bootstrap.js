f3165e3d35a3d5b904ec6b85a4549c39d8f215e7
MyMess-Siberia <siberiamess6@gmail.com>
2024-11-23 14:08:43 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2540632187"
+    serviceWorkerVersion: "1957791978"
   }
 });
